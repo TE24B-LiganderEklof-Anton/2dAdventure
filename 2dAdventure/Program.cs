63eb5463211b78520c0d48a7cec96ab42f7a3ebc
TE24B-Ligandereklof-Anton <anton.ligandereklof@elev.ga.ntig.se>
2025-12-01 11:03:48 +0100

    Jobbat på terränggenereringen. Den funkar i stor del nu men kan fortfarande behöva tweakas lite.

--- 2dAdventure/Program.cs
+++ 2dAdventure/Program.cs
@@ -54,10 +54,10 @@ while (!Raylib.WindowShouldClose())
 class Tile
 {
     public Terrain terrain;
-    public Structure structure;
+    // public Structure structure;
 }
 enum Structure
-{
+{ 
     hut,
     ruins,
     campfire,
@@ -66,25 +66,28 @@ enum Structure
 class Terrain
 {
     public Color tileColor = Color.Pink;
-    public Structure[] validStructures;
+    // public Structure[] validStructures;
     public bool traversable = true;
 }
 
 class World
 {
     //terrain classes
+    Terrain debug = new();
     Terrain plains = new();
     Terrain water = new();
     Terrain mountains = new();
 
     public World()
     {
-        plains.tileColor = Color.Green;
+        debug.tileColor = Color.Purple;
+
+        plains.tileColor = new(0, 255, 0);
 
         water.tileColor = Color.Blue;
         water.traversable = false;
 
-        mountains.tileColor = Color.Gray;
+        mountains.tileColor = Color.Gray; 
     }
     
 
@@ -102,8 +105,14 @@ class World
     }
     void DrawTile(int yPos, int xPos, Tile tile)
     {
+        int randomRGB = 10;
         (int graphicsYPos, int graphicsXPos) = GetTileGraphicsPos(yPos, xPos);
-        Color color = tile.terrain.tileColor;
+        Color color = Color.Pink;
+        if (tile.terrain != null)
+        {
+            // Color randomizedColor = new(color.R + Random.Shared.Next(-randomRGB, randomRGB), color.G + Random.Shared.Next(-randomRGB, randomRGB), color.B + Random.Shared.Next(-randomRGB, randomRGB));
+            color = tile.terrain.tileColor;
+        }
 
         Raylib.DrawRectangle(graphicsXPos, graphicsYPos, tileSize, tileSize, color);
     }
@@ -164,12 +173,12 @@ class World
         
     }
 
-    public List<Tile> GetNeighboringTiles(int yPos, int xPos)
+    public List<Tile> GetNeighboringTiles(int yPos, int xPos, int radius)
     {
         List<Tile> tiles = new();
-        for (int y = yPos-1; y <= yPos+1; y++)
+        for (int y = yPos-radius; y <= yPos+radius; y++)
         {
-            for (int x = xPos-1; x <= xPos+1; x++)
+            for (int x = xPos-radius; x <= xPos+radius; x++)
             {
                 if(y == yPos && x == xPos)
                 {
@@ -185,43 +194,111 @@ class World
     {
         Dictionary<int, Tile> xAxis = GetXAxisDict(y);
         Tile newTile = new();
+        
 
-        Dictionary<Terrain, int> terrainValues = new()
+        Dictionary<Terrain, float> terrainValues = new()
         {
-            {water, 1},
-            {plains, 3},
-            {mountains, 1},
+            {water, 0.1f},
+            {plains, 0.5f},
+            {mountains, 0.7f},
+            {debug, 0}
+        };
+        Dictionary<Terrain, float> neighboringBonus = new()
+        {
+            {water, 0.75f},
+            {plains, 0.5f},
+            {mountains, -0.2f},
+            {debug, 1}
         };
 
-        List<Tile> neighboringTiles = GetNeighboringTiles(y, x);
+        List<Tile> neighboringTiles = GetNeighboringTiles(y, x, 1);
 
         foreach (Tile tile in neighboringTiles)
         {
+            if (tile == null) continue;
+
             Terrain terrain = tile.terrain;
-            terrainValues[terrain] += 1;
+            terrainValues[terrain] += neighboringBonus[terrain];
+            if (terrain == water)
+            {
+                terrainValues[mountains] *= 0.1f;
+            }
+            if (terrain == mountains)
+            {
+                terrainValues[water] *= 0.1f;
+            }
         }
 
+        float totalTerrainValue = 0;
+        foreach (KeyValuePair<Terrain,float> pair in terrainValues)
+        {
+            // Console.WriteLine(pair.Value);
+            totalTerrainValue += pair.Value;
+        }
+        // Console.WriteLine(totalTerrainValue);
+        // Console.WriteLine("////////////////////////////////////////////////////");
+
+        float selectionValue = (float)Random.Shared.NextDouble() * totalTerrainValue;
+        // Console.WriteLine(selectionValue);
+
+        Terrain selectedTerrain = plains;
+
+        foreach (KeyValuePair<Terrain,float> pair in terrainValues)
+        {
+            totalTerrainValue -= pair.Value;
+            if (totalTerrainValue < selectionValue)
+            {
+                Console.WriteLine(pair.Key);
+                selectedTerrain = pair.Key; //issue is here
+                // Console.WriteLine("break");
+                break;
+            }
+        }
+        Console.WriteLine("==========================================");
         
+        //  if (selectedTerrain == water)
+        // {
+        //     Console.WriteLine("water");
+        // }
+        // else if (selectedTerrain == debug)
+        // {
+        //     Console.WriteLine("debug");
+        // }
+        // else if (selectedTerrain == plains)
+        // {
+        //     Console.WriteLine("plains");
+        // }
+        // else if (selectedTerrain == null)
+        // {
+        //     Console.WriteLine("null");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("none");
+        // }
+        
+
+        newTile.terrain = selectedTerrain;
         // Console.WriteLine(neighboringTiles.Count);
+        // Console.WriteLine(selectedTerrain);
 
 
 
+        // // //will be replaced with a proper algoritm, random for now
+        // int terrainType = Random.Shared.Next(3);
 
-        //will be replaced with a proper algoritm, random for now
-        int terrainType = Random.Shared.Next(3);
-
-        if (terrainType == 0)
-        {
-            newTile.terrain = water;
-        }
-        else if (terrainType == 1)
-        {
-            newTile.terrain = plains;
-        }
-        else if (terrainType == 2)
-        {
-            newTile.terrain = mountains;
-        }
+        // if (terrainType == 0)
+        // {
+        //     newTile.terrain = water;
+        // }
+        // else if (terrainType == 1)
+        // {
+        //     newTile.terrain = plains;
+        // }
+        // else if (terrainType == 2)
+        // {
+        //     newTile.terrain = mountains;
+        // }
 
         xAxis.Add(x, newTile);
 
--- 2dAdventure/obj/Debug/net9.0/2dAdventure.AssemblyInfo.cs
+++ 2dAdventure/obj/Debug/net9.0/2dAdventure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2dAdventure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc71f9f3407be43aae4dbe22ffd3541f85ceef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8086d8ae21e9357a1de0e18301e17bce8354998")]
 [assembly: System.Reflection.AssemblyProductAttribute("2dAdventure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2dAdventure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
